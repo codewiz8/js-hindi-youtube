9f6330325602cbc3c36b2d068c91c57e3df4b781
codewiz8 <riyadas2xx@gmail.com>
2026-01-06 06:10:10 +0000

    02_datatypes.js

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3); console.log("Riya")
+
+let name = "hitesh"
+let age = 20
+let isLoggedIn = false
+let state = null
+
+// number => 2 to pwer 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+
+console.log(typeof undefined);
